59be5665a0eb4a8824a66e396cadaf07b310ab97
rosypen <rosy.pen.1@gmail.com>
2016-10-02 08:03:29 -0700

    Renaming Extension

---
+++ Hack_Hate_Extension_accordian/accordion.js
@@ -0,0 +1,20 @@
+//This Javascript is supposed to make the accordions collapse and expand
+//The active attribute makes the +/- symbols (according to the css)
+//The show attribute displays the panel (hidden text)
+
+//Currently, it does not work with on the extension
+document.addEventListener('DOMContentLoaded', accordian() {
+  //document.querySelector('a').addEventListener('click', accordian);
+
+  function accordian() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].onclick = function(){
+          this.classList.toggle("active");
+          this.nextElementSibling.classList.toggle("show");
+        };
+      };
+    };
+});
